ab27a5cf496a2d6dab54025405413bee2adf6e3a
Ian Clarkson <clarksonian@gmail.com>
2016-05-27 15:42:32 -0400

    Compiles but doesn't do much

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,12 @@ use rand::distributions::{IndependentSample, Range};
 
 fn dice_roll(sides: u8) -> u8 {
 //	return rand::random::<u8>() % 6;
-
-let between = Range::new(0,sides);
-let mut rng = rand::thread_rng();
-return between.ind_sample(&mut rng);
+	let between = Range::new(0,sides);
+	let mut rng = rand::thread_rng();
+	return between.ind_sample(&mut rng);
 }
 
-
-
 fn main() {
-let mut sm_hlth : i32
-let mut sm_str : i32
+	let mut sm_hlth : i32;
+	let mut sm_str : i32;
 }
