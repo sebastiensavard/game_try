05751a64c936af543eaba78e1da5d76f8538bf22
Ian Clarkson <clarksonian@gmail.com>
2016-05-27 15:59:15 -0400

    Showing space marine health and strength

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,13 @@ use rand::distributions::{IndependentSample, Range};
 
 fn dice_roll(sides: u8) -> u8 {
 //	return rand::random::<u8>() % 6;
-	let between = Range::new(0,sides);
+	let between = Range::new(0, sides);
 	let mut rng = rand::thread_rng();
 	return between.ind_sample(&mut rng);
 }
 
 fn main() {
-	let mut sm_hlth : i32;
-	let mut sm_str : i32;
+	let mut sm_hlth = 100;
+	let mut sm_str = dice_roll(11) + 10;
+	println!("Space marine has health {} and strength {}", sm_hlth, sm_str);
 }
